330ee160400dc4e063131f70e0487f194d3407c0
Excel Bill <60159086+Kessyblaise1@users.noreply.github.com>
2021-10-26 21:50:17 +0100

    added cart functionality

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ const slider = tns({
     nextButton: '.next',
     prevButton: '.prev'
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    let cartButton = document.querySelector('.cart')
+    let cart = document.querySelector('.cart_container')
+    let space = document.querySelectorAll('section')
+
+    space.forEach(element => {
+        element.addEventListener('click', () => {
+            cart.classList.remove('show_cart')
+            cartButton.classList.remove("active")
+        })
+    })
+
+    cartButton.addEventListener('click', () => {
+        cart.classList.toggle("show_cart")
+        cartButton.classList.toggle("active")
+    })
+
+    (function () {
+        
+    })();
+})
